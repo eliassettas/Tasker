26fd86574d281f85ab2c0a8b5ce6517a4fd51690
Elias Settas <elias.settas@gmail.com>
2024-10-20 12:10:38 +0300

    Display previous message history on chat

--- src/main/java/com/example/tasker/service/ChatMessageService.java
+++ src/main/java/com/example/tasker/service/ChatMessageService.java
@@ -1,20 +1,47 @@
 package com.example.tasker.service;
 
+import java.util.Collections;
+import java.util.List;
+
+import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 
+import com.example.tasker.exception.CustomException;
 import com.example.tasker.model.message.ChatMessage;
+import com.example.tasker.model.persistence.SystemUser;
+import com.example.tasker.model.persistence.UserProfile;
 import com.example.tasker.repository.ChatMessageRepository;
+import com.example.tasker.util.UserUtils;
 
 @Service
 public class ChatMessageService {
 
     private final ChatMessageRepository chatMessageRepository;
+    private final SystemUserService systemUserService;
 
-    public ChatMessageService(ChatMessageRepository chatMessageRepository) {
+    public ChatMessageService(
+            ChatMessageRepository chatMessageRepository,
+            SystemUserService systemUserService
+    ) {
         this.chatMessageRepository = chatMessageRepository;
+        this.systemUserService = systemUserService;
     }
 
     public ChatMessage save(ChatMessage chatMessage) {
         return chatMessageRepository.save(chatMessage);
+    }
+
+    public List<ChatMessage> getPreviousMessages(String currentMessageId) throws CustomException {
+        List<ChatMessage> messages = currentMessageId != null && !currentMessageId.isEmpty()
+                ? chatMessageRepository.findTop20ByIdIsLessThanOrderByIdDesc(new ObjectId(currentMessageId))
+                : chatMessageRepository.findTop20ByOrderByIdDesc();
+        Collections.reverse(messages);
+        for (ChatMessage message : messages) {
+            SystemUser systemUser = systemUserService.getUserById(message.getSenderId());
+            UserProfile userProfile = systemUser.getProfile();
+            String name = UserUtils.constructName(userProfile.getFirstName(), userProfile.getLastName());
+            message.setSenderName(name);
+        }
+        return messages;
     }
 }
